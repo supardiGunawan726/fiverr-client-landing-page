46a4c678134beabdcf95b5311b2decd7550bcbc9
supardiGunawan726 <xblade131@gmail.com>
2022-01-12 15:51:01 +0700

    Project Initialization

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Footer: FC = () => {
+  return <div>ini footer</div>;
+};
+
+export default Footer;
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Navbar: FC = () => {
+  return <div className="text-red-500">ini navbar</div>;
+};
+
+export default Navbar;
